72b1931f9a526f7e09a09a838db9477c0f1b9d80
User <User@DESKTOP-2ALLHRU>
2018-09-04 14:50:54 +0300

    Ctrl+Shift+F

--- Homework1b/src/com/joinz/homework1b/Main.java
+++ Homework1b/src/com/joinz/homework1b/Main.java
@@ -4,14 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		
 		double side1 = 0.3;
 		double side2 = 0.4;
 		double side3 = 0.5;
 		double p = (side1 + side2 + side3) * 0.5;
 		double result = Math.sqrt(p * (p - side1) * (p - side2) * (p - side3));
 		System.out.println(result);
-		
 
 	}
 
