e6c76f12cbf8dcdfa991dd909b2d335b5442fe2d
User <User@DESKTOP-2ALLHRU>
2018-09-04 15:55:43 +0300

    Verifications added

--- Homework1a/src/com/joinz/homework1a/Main.java
+++ Homework1a/src/com/joinz/homework1a/Main.java
@@ -8,23 +8,37 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
+		int number;
 		System.out.println("Input 5-digit number");
-		int number = sc.nextInt();
+		if (sc.hasNextInt()) {
+			number = sc.nextInt();
 
-		int char1 = number / 10000;
-		String ch1 = char1 + "\n";
-		int char2 = number % 10000 / 1000;
-		String ch2 = char2 + "\n";
-		int char3 = number % 1000 / 100;
-		String ch3 = char3 + "\n";
-		int char4 = number % 100 / 10;
-		String ch4 = char4 + "\n";
-		int char5 = number % 10;
-		String ch5 = char5 + "\n";
+			if ((number / 10000) >= 1) {
+				if ((number / 10000 <= 9)) {
+					int char1 = number / 10000;
+					String ch1 = char1 + "\n";
+					int char2 = number % 10000 / 1000;
+					String ch2 = char2 + "\n";
+					int char3 = number % 1000 / 100;
+					String ch3 = char3 + "\n";
+					int char4 = number % 100 / 10;
+					String ch4 = char4 + "\n";
+					int char5 = number % 10;
+					String ch5 = char5 + "\n";
+		
+					String result = ch1 + ch2 + ch3 + ch4 + ch5;
+					System.out.println(result);
+				} else { 
+					System.out.println("You did not enter a 5-digit number");
+				}
+			} else { 
+				System.out.println("You did not enter a 5-digit number");
+			}
+		} else {
+			System.out.println("You did not enter an integer");
+		}
 
-		String result = ch1 + ch2 + ch3 + ch4 + ch5;
-		System.out.println(result);
-
+		sc.close();
 	}
 
 }
