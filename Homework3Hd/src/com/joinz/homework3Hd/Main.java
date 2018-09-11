d6b5ce4d8e6daf2ee46a1a18c26b798da3693155
User <User@DESKTOP-2ALLHRU>
2018-09-11 11:03:49 +0300

    Verification added

--- Homework3Hd/src/com/joinz/homework3Hd/Main.java
+++ Homework3Hd/src/com/joinz/homework3Hd/Main.java
@@ -8,13 +8,15 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Input 6-digit number");
 		int n = sc.nextInt();
-		int n6 = n / 100000;
-		int n5 = n % 100000 / 10000;
-		int n4 = n % 10000 / 1000;
-		int n3 = n % 1000 / 100;
-		int n2 = n % 100 / 10;
-		int n1 = n % 10;
-		if (n1 == n6 && n2 == n5 && n3 == n4) {
+		int n1 = n / 100000;
+		int n2 = n % 100000 / 10000;
+		int n3 = n % 10000 / 1000;
+		int n4 = n % 1000 / 100;
+		int n5 = n % 100 / 10;
+		int n6 = n % 10;
+		if ((n / 100000 > 9) || (n / 100000 == 0)) {
+			System.out.println("Wrong number!");
+		}else if (n1 == n6 && n2 == n5 && n3 == n4) {
 			System.out.println("It is palendrom!");
 		} else {
 			System.out.println("It isn't palendrom");
