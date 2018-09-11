61fe0e04e9ef3ee5ccef3b7a55e5b42277a3d5ac
User <User@DESKTOP-2ALLHRU>
2018-09-11 10:16:22 +0300

    First Commit

---
+++ Homework3Hd/src/com/joinz/homework3Hd/Main.java
@@ -0,0 +1,26 @@
+package com.joinz.homework3Hd;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input 6-digit number");
+		int n = sc.nextInt();
+		int n6 = n / 100000;
+		int n5 = n % 100000 / 10000;
+		int n4 = n % 10000 / 1000;
+		int n3 = n % 1000 / 100;
+		int n2 = n % 100 / 10;
+		int n1 = n % 10;
+		if (n1 == n6 && n2 == n5 && n3 == n4) {
+			System.out.println("It is palendrom!");
+		} else {
+			System.out.println("It isn't palendrom");
+		}
+		
+		sc.close();
+	}
+
+}
