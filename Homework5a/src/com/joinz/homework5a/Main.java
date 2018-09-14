ae50acc04667a4de0faf95ff147e00a215156295
User <User@na1r.services.adobe.com>
2018-09-14 17:43:21 +0300

    First Commit

--- Homework5a/src/com/joinz/homework5a/Main.java
+++ Homework5a/src/com/joinz/homework5a/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		int [] mas = { 0, 5, 2, 4, 7, 1, 3, 19 };
+		
 		int c = 0;
 		for (int i = 0; i < mas.length; i++) {
 			if (mas[i] % 2 != 0) {
@@ -11,6 +12,7 @@ public class Main {
 			}
 		}
 		System.out.println(c);
+		
 		c = 0;
 		for (int i : mas) {
 			if (i % 2 != 0) {
