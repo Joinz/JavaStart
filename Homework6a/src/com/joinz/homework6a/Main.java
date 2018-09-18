3693c62d1190d2341b9e5723ce6ba49e60acc6cc
User <User@na1r.services.adobe.com>
2018-09-18 17:37:15 +0300

    First Commit

---
+++ Homework6a/src/com/joinz/homework6a/Main.java
@@ -0,0 +1,35 @@
+package com.joinz.homework6a;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Random rnd = new Random();
+		int[] mas = new int[10];
+		for (int i = 0; i < mas.length; i++) {
+			mas[i] = rnd.nextInt(50);
+		}
+		System.out.println(Arrays.toString(mas));
+		System.out.println("Max integer is " + max(mas));
+		
+	}
+
+	/**
+	 * Search maximum integer of massive integers.
+	 * 
+	 * @param array <code>array</code> which need to search.
+	 * @return Maximum integer value.
+	 */
+	static int max(int[] array) {
+		int max = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (max <= array[i]) {
+				max = array[i];
+			}
+		}
+		return max;
+	}
+}
