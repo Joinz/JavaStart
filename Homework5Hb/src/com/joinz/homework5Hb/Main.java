99c785dffb31bb7c8100b33a5badadb7944dff5c
Joinz <joinz@mail.ru>
2018-11-17 16:13:04 +0200

    Complete.

--- Homework5Hb/src/com/joinz/homework5Hb/Main.java
+++ Homework5Hb/src/com/joinz/homework5Hb/Main.java
@@ -7,8 +7,12 @@ public class Main {
 	public static void main(String[] args) {
 
 		int[] mas = { 4, 5, 10, 1, 5 };
-		for (int i = 0; i < mas.length; i++) {
-			mas[i] = mas[mas.length - (i + 1)];
+		int temp;
+		
+		for (int i = 0; i < mas.length/2; i++) {
+			temp = mas[i];
+			mas[i] = mas[mas.length - (i+1)];
+			mas[mas.length - (i+1)] = temp;
 		}
 		System.out.println(Arrays.toString(mas));
 	}
