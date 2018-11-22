daf2e9a875b5bb6125088d509b0fc59ba41bd175
Joinz <joinz@mail.ru>
2018-11-22 21:15:57 +0200

    First Commit

--- Homework1a/src/com/joinz/homework1a/Main.java
+++ Homework1a/src/com/joinz/homework1a/Main.java
@@ -7,26 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
-<<<<<<< HEAD
-		System.out.println("Input 5-digit number");
-		int number = sc.nextInt();
-
-		int char1 = number / 10000;
-		String ch1 = char1 + "\n";
-		int char2 = number % 10000 / 1000;
-		String ch2 = char2 + "\n";
-		int char3 = number % 1000 / 100;
-		String ch3 = char3 + "\n";
-		int char4 = number % 100 / 10;
-		String ch4 = char4 + "\n";
-		int char5 = number % 10;
-		String ch5 = char5 + "\n";
-
-		String result = ch1 + ch2 + ch3 + ch4 + ch5;
-		System.out.println(result);
-
-=======
 		int number;
 		System.out.println("Input 5-digit number");
 		if (sc.hasNextInt()) {
@@ -58,7 +38,6 @@ public class Main {
 		}
 
 		sc.close();
->>>>>>> refs/remotes/origin/master
 	}
 
 }
--- Homework3a/src/com/joinz/homework3a/Main.java
+++ Homework3a/src/com/joinz/homework3a/Main.java
@@ -3,7 +3,6 @@ package com.joinz.homework3a;
 import java.util.Scanner;
 
 public class Main {
-<<<<<<< HEAD
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -31,31 +30,5 @@ public class Main {
 		System.out.println("Max = " + max);
 		
 		sc.close();
-
 	}
-
-=======
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Input any 4 numbers");
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		int d = sc.nextInt();
-		int max = a;
-		if (b > max) {
-			max = b;
-		}
-		if (c > max) {
-			max = c;
-		}
-		if (d > max) {
-			max = d;
-		}
-		System.out.println(max);
-		
-		sc.close();
-	}
->>>>>>> refs/remotes/origin/master
 }
