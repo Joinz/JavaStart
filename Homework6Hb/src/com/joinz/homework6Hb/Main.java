f83694742f248c9d949b164b0cb943e4648ede5a
Joinz <joinz@mail.ru>
2018-11-22 21:24:47 +0200

    Max palyndrome using numbers '999' is 906609 = 993 * 913. Error with number system.

--- Homework6Hb/src/com/joinz/homework6Hb/Main.java
+++ Homework6Hb/src/com/joinz/homework6Hb/Main.java
@@ -4,25 +4,30 @@ package com.joinz.homework6Hb;
 public class Main {
 	public static void main (String[] args) {
 		int n = 999;
-		System.out.println("Max palyndrome using numbers '" + n + "' is " + getPalyndrome(n));
+		System.out.println(getPalyndrome(n));
 //		System.out.println(checkPalyndrome(999));
 //		System.out.println(checkPalyndrome(11011));
 //		System.out.println(checkPalyndrome(9009));
-//		System.out.println(checkPalyndrome(0220));
+//		System.out.println(checkPalyndrome(0220)); почему-то переводит в восьмеричную
+//		систему измерения некоторые числа (напр. 0220 в число 144)
 	}
 	
-	public static int getPalyndrome(int n) {
+	public static String getPalyndrome(int n) {
 		int number = -1;
+		int maxI = -1;
+		int maxJ = -1;
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
 				if (checkPalyndrome(i*j)) {
 					if (number <= i * j) {
 						number = i*j;
+						maxI = i;
+						maxJ = j;
 					}
 				}
 			}
 		}
-		return number;
+		return "Max palyndrome using numbers '" + n + "' is " + number + " = " + maxI + " * " + maxJ;
 	}
 	
 	public static boolean checkPalyndrome(int n) {
